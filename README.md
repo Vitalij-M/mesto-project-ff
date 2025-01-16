29207ae1d98309bdf7550535ebb5c83a7d206c89
VitM <vitmikhajlov@yandex.ru>
2025-01-16 21:48:13 +0300

    16.01.25

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Vitalij-M/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const cardContainer = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function createCard(card, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__title").textContent = card.name;
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt = card.name;
 
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => deleteCard(cardElement));
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(item) {
+  item.remove();
+}
 // @todo: Вывести карточки на страницу
+
+function renderCard(cardElement) {
+  cardContainer.append(cardElement);
+}
+
+initialCards.forEach(function(card) {
+  renderCard(createCard(card, deleteCard));
+});
